2922639d03edca67b29cac0a09096c4368943ce8
Jiankang Cheng <poiuy_5@163.com>
2022-08-28 22:15:46 +0800

    sss

--- src/main/java/com/github/lyrric/Main.java
+++ src/main/java/com/github/lyrric/Main.java
@@ -3,17 +3,6 @@ package com.github.lyrric;
 import com.github.lyrric.ui.ConsoleMode;
 import com.github.lyrric.ui.MainFrame;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
-import java.text.ParseException;
-
-/**
- * Created on 2020-07-21.
- *
- * @author wangxiaodong
- */
-public class Main {
 
     private  static  final Logger logger = LogManager.getLogger(Main.class);
 
